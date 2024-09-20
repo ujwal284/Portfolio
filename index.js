aaad44af31bf3e3094bb4c0f3f8750cf4aaa7840
ujwal284 <ujwalkhanal284@gmail.com>
2024-09-20 12:24:52 +0545

    apply responsive for website with hamburger icon  and created js files

---
+++ index.js
@@ -0,0 +1,7 @@
+// JavaScript for burger menu toggle
+const burger = document.getElementById('burger');
+const navLinks = document.getElementById('nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
